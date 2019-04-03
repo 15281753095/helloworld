7252693be7b72d279ee2bf0fdd4a0533534e2a59
15281753095 <1762049648@qq.com>
2019-04-03 18:23:57 +0800

    test

---
+++ src/Test0.java
@@ -0,0 +1,8 @@
+public class Test0 {
+    public static void main(String [] args){
+        System.out.print("Hello World");
+        char s = 0x94C1;
+        char b = 0x61A8;
+        System.out.print(s+""+b+b);
+    }
+}
